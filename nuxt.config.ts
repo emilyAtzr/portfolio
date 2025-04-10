53fddd2f6228bda033efd3dbef4c22873184627c
Emily Atzinger <emily.atzinger@htlstp.at>
2025-04-10 10:43:11 +0200

    added tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/fonts',
     '@nuxt/icon'
-  ]
-})
+  ],
+});
